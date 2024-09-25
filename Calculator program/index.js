03fbd9a159b669737c316c49e2c3a7955cebb5bd
Defenseman <worknesterov@yandex.kz>
2024-09-25 22:58:52 +0500

    Bro Code JS Full Course 58/75 Calculator 2.0

--- Calculator program/index.js
+++ Calculator program/index.js
@@ -1,15 +1,43 @@
 const display = document.getElementById("display");
+/*function isOperator(char) {
+    return ['+', '-', '*', '/', '%']
+}*/
 
 function append(input) {
+    let currentValue = display.value;
+
+    if(currentValue === "ERROR" || currentValue.length === 11) {
+        return;
+    }if(currentValue === '0' /*&& !isOperator(input)*/) {
+        return display.value = input ;
+    }/*if(isOperator(currentValue.slice(-1)) && isOperator(input)) {
+        return display.value = currentValue.slice(0, -1) + input;
+    }*/
     display.value += input
+
 }
-function clear() {
-    display.value = "";
+function clearDisplay() {
+    let currentValue = display.value;
+    display.value = '0';
 }
 function result() {
+    let currentValue = display.value;
+    if(currentValue.trim() === '') {
+        return display.value = '';
+    }
     try {
         display.value = eval(display.value)
     }catch (error) {
         display.value = 'ERROR'
+    }
+}
+function change() {
+    let currentValue = display.value
+    if (currentValue !== '' && !isNaN(Number(currentValue))) {
+        if (currentValue.startsWith('-')) {
+            display.value = currentValue.slice(1);
+        }else {
+            display.value = '-' + currentValue;
+        }
     }
 }
