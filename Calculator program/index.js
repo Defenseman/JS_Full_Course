9801be9c85224dc89b532f98b4045983a4bd34cf
Defenseman <worknesterov@yandex.kz>
2024-09-26 20:32:48 +0500

    Bro Code JS Full Course 58/75 Calculator 3.0

--- Calculator program/index.js
+++ Calculator program/index.js
@@ -1,43 +1,61 @@
 const display = document.getElementById("display");
-/*function isOperator(char) {
-    return ['+', '-', '*', '/', '%']
-}*/
+
+function isOperator(char) {
+    return ['+', '-', '*', '/', '%'].includes(char)
+}
 
 function append(input) {
     let currentValue = display.value;
 
-    if(currentValue === "ERROR" || currentValue.length === 11) {
+    if (currentValue === "ERROR" || currentValue.length === 11) {
         return;
-    }if(currentValue === '0' /*&& !isOperator(input)*/) {
-        return display.value = input ;
-    }/*if(isOperator(currentValue.slice(-1)) && isOperator(input)) {
+    }
+    if (currentValue === '0' && !isOperator(input)) {
+        return display.value = input;
+    }
+    if (isOperator(currentValue.slice(-1)) && isOperator(input)) {
         return display.value = currentValue.slice(0, -1) + input;
-    }*/
+    }
     display.value += input
-
 }
+
 function clearDisplay() {
-    let currentValue = display.value;
     display.value = '0';
 }
+
 function result() {
     let currentValue = display.value;
-    if(currentValue.trim() === '') {
-        return display.value = '';
+    if (currentValue.trim() === '' || !/[0-9]/.test(currentValue) || currentValue.slice(-1) === '-' || currentValue.slice(-1) === '+' || currentValue.slice(-1) === '%' || currentValue.slice(-1) === '*' || currentValue.slice(-1) === '/') {
+        return;
+    }
+    if (!currentValue.includes('+') && !currentValue.includes('-') && !currentValue.includes('*') && !currentValue.includes('/') && !currentValue.includes('%')) {
+        display.value = display.value * 2
     }
     try {
         display.value = eval(display.value)
-    }catch (error) {
+    } catch (error) {
         display.value = 'ERROR'
     }
 }
+
 function change() {
     let currentValue = display.value
     if (currentValue !== '' && !isNaN(Number(currentValue))) {
         if (currentValue.startsWith('-')) {
             display.value = currentValue.slice(1);
-        }else {
+        } else {
             display.value = '-' + currentValue;
         }
     }
+}
+
+function dotDisplay(input) {
+    let currentValue = display.value;
+    let part = currentValue.split(/[+\-*\/%]/);
+    let lastPart = part[part.length - 1];
+
+    if (lastPart.includes('.') || currentValue === '' || isOperator(currentValue.slice(-1)) || currentValue === 'ERROR') {
+        return;
+    }
+    display.value += input
 }
