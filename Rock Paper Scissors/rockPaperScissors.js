61c33e84e2b1304feb05baeb7bc6c55e315c88cb
Defenseman <worknesterov@yandex.kz>
2024-10-10 21:03:21 +0500

    Bro Code JS Full Course 68/75 Rock Paper Scissors Final

--- Rock Paper Scissors/rockPaperScissors.js
+++ Rock Paper Scissors/rockPaperScissors.js
@@ -1,28 +1,49 @@
 const choices = ['rock', 'paper', 'scissors']
-const playerResult = document.getElementById("resultDisplay");
-const computerResult = document.getElementById("computerDisplay");
+const playerResult = document.getElementById("playerResult");
+const computerResult = document.getElementById("computerResult");
 const resultDisplay = document.getElementById("resultDisplay");
+
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+let playerScore = 0;
+let computerScore = 0;
 
 
 function playGame(playerChoice) {
     const computerChoice = choices[Math.floor(Math.random() * choices.length)];
     let result = "";
-    console.log(computerChoice)
+
     if (computerChoice === playerChoice ) {
-        resultDisplay.textContent = 'It`s a Tie';
+        result = 'It`s a TIE';
     }else {
         switch (playerChoice) {
             case "rock":
-                (computerChoice === 'scissors') ? 'You WIN' : 'You LOSE';
+                result = (computerChoice === 'scissors') ? 'YOU WIN' : 'YOU LOSE';
                 break
             case "paper":
-                (computerChoice === 'rock') ? 'You WIN' : 'You LOSE';
+                result = (computerChoice === 'rock') ? 'YOU WIN' : 'YOU LOSE';
                 break
             case "scissors":
-                computerChoice === 'scissors' ? 'You WIN' : 'You LOSE';
+                result = computerChoice === 'scissors' ? 'YOU WIN' : 'YOU LOSE';
                 break
         }
     }
     playerResult.textContent = `PLAYER ${playerChoice}`;
     computerResult.textContent = `COMPUTER ${computerChoice}`;
+    resultDisplay.textContent = result
+
+    resultDisplay.classList.remove('greenText', 'redText')
+
+    switch (result) {
+        case "YOU WIN":
+            resultDisplay.classList.add('greenText');
+            playerScore++
+            playerScoreDisplay.textContent = playerScore;
+            break
+        case "YOU LOSE":
+            resultDisplay.classList.add('redText')
+            computerScore++
+            computerScoreDisplay.textContent = computerScore;
+            break
+    }
 }
